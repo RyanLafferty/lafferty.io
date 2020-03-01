a2aff804fd95c3a9149cf9a01c160b0fc98a4dad
RyanLafferty <rlafferty1@hotmail.com>
2020-03-01 16:54:52 -0500

    Init contact page.

---
+++ src/views/Contact/Contact.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <h1>{{ content.en.title }}</h1>
+  </div>
+</template>
+
+<script>
+import content from './Contact.json';
+
+export default {
+  data: () => ({
+    content,
+  }),
+};
+</script>
+
+<style lang="scss" scoped>
+  @import 'Contact.scss'
+</style>

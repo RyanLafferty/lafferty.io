5c1987b2f898be94413c9c10c1373bd474e501d3
RyanLafferty <rlafferty1@hotmail.com>
2020-03-01 23:06:40 -0500

    Init skill component

---
+++ src/components/Skill/Skill.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <component :is="logo" class="logo"/>
+    <span class="label">
+      {{ label }}
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    logo: {
+      type: Object,
+      default: null,
+    },
+    label: {
+      type: String,
+      default: '',
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  @import 'Skill.scss'
+</style>

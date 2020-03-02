f49b7fba3978cd42a17fdf791cf06537c372d074
RyanLafferty <rlafferty1@hotmail.com>
2020-03-01 21:57:20 -0500

    Init nav bar

---
+++ src/components/NavBar/NavBar.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <NavBarOption
+      v-for="option in options"
+      :key="`nav-bar-option-${option.label}`"
+      :link="option.link"
+      :label="option.label"
+    >
+  </div>
+</template>
+
+<script>
+import NavBarOption from '@/components/NavBarOption';
+
+export default {
+  props: {
+    options: {
+      type: Array,
+      default: () => [{
+        link: '/',
+        label: 'Home',
+      }],
+    },
+    components: {
+      NavBarOption,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  @import 'NavBar.scss'
+</style>
---
+++ src/components/NavBarOption/NavBarOption.vue
@@ -0,0 +1,26 @@
+<template>
+  <router-link :to="link">
+    <div class="container">
+      {{label}}
+    </div>
+  </router-link>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: 'Home',
+    },
+    link: {
+      type: String,
+      default: '/',
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  @import 'NavBarOption.scss'
+</style>

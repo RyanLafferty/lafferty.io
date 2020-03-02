4d124ca42d89d87fe5a75086bcfd80555153e31b
RyanLafferty <rlafferty1@hotmail.com>
2020-03-01 23:07:14 -0500

    Init Backend Skills component

---
+++ src/components/BackendSkills/BackendSkills.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <Skill :logo="skills[0].logo" :label="skills[0].label"/>
+  </div>
+</template>
+
+<script>
+import Skill from '@/components/Skill';
+import { BackendSkills as skills } from '@/assets/config/Skills';
+
+export default {
+  data: () => ({
+    skills,
+  }),
+  components: {
+    Skill,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  @import 'BackendSkills.scss'
+</style>

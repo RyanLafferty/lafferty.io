98c989f6ccd88ee29e8b587192c2e4c519d3b032
RyanLafferty <rlafferty1@hotmail.com>
2020-03-01 23:06:19 -0500

    Added ruby icon. Init skills config.

---
+++ src/assets/config/Skills.ts
@@ -0,0 +1,9 @@
+import RubyLogo from '@/assets/svg/ruby.svg';
+
+// eslint-disable-next-line import/prefer-default-export
+export const BackendSkills = [
+  {
+    label: 'Ruby',
+    logo: RubyLogo,
+  },
+];

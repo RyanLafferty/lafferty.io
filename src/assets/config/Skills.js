3f4fe55317afbc87aa9d3724c920a13b1945b489
RyanLafferty <rlafferty1@hotmail.com>
2020-03-02 21:50:02 -0500

    Moving some of the logos to another directory

--- src/assets/config/Skills.js
+++ src/assets/config/Skills.js
@@ -1,6 +1,6 @@
-import RubyLogo from '@/assets/svg/ruby.svg';
-import PythonLogo from '@/assets/svg/python.svg';
-import NodeJSLogo from '@/assets/svg/nodejs.svg';
+import RubyLogo from '@/assets/svg/logos/monochrome/ruby.svg';
+import PythonLogo from '@/assets/svg/logos/monochrome/python.svg';
+import NodeJSLogo from '@/assets/svg/logos/monochrome/nodejs.svg';
 
 // eslint-disable-next-line import/prefer-default-export
 export const BackendSkills = [

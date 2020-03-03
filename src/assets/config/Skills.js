87e65dec1b7ded3a46bd8e14ba558834a9c6ecbf
RyanLafferty <rlafferty1@hotmail.com>
2020-03-02 20:36:54 -0500

    Added new skill icons

--- src/assets/config/Skills.js
+++ src/assets/config/Skills.js
@@ -1,9 +1,19 @@
 import RubyLogo from '@/assets/svg/ruby.svg';
+import PythonLogo from '@/assets/svg/python.svg';
+import NodeJSLogo from '@/assets/svg/nodejs.svg';
 
 // eslint-disable-next-line import/prefer-default-export
 export const BackendSkills = [
   {
     label: 'Ruby',
     logo: RubyLogo,
+  },
+  {
+    label: 'Python',
+    logo: PythonLogo,
+  },
+  {
+    label: 'Node.js',
+    logo: NodeJSLogo,
   },
 ];

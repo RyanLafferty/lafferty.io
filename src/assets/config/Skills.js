91f231423b5f038c7c21b5fc702c191d7144535f
RyanLafferty <rlafferty1@hotmail.com>
2020-03-04 21:44:54 -0500

    Added es6 skill. Init frontend skills.

--- src/assets/config/Skills.js
+++ src/assets/config/Skills.js
@@ -3,6 +3,9 @@ import RubyLogo from '@/assets/svg/logos/colored/ruby.svg';
 import PythonLogo from '@/assets/svg/logos/colored/python.svg';
 import NodeJSLogo from '@/assets/svg/logos/colored/nodejs.svg';
 
+// Frontend Logos
+import ES6Logo from '@/assets/svg/logos/colored/es6.svg';
+
 // Platform Logos
 import AWSLogo from '@/assets/svg/logos/colored/aws.svg';
 import KubernetesLogo from '@/assets/svg/logos/colored/kubernetes.svg';
@@ -25,7 +28,12 @@ export const BackendSkills = [
   },
 ];
 
-export const FrontendSkills = [];
+export const FrontendSkills = [
+  {
+    label: 'ES6',
+    logo: ES6Logo,
+  },
+];
 
 export const PlatformSkills = [
   {

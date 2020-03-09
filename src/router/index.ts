87c254242bbc32c89f170910b40db09beb6ae8ba
RyanLafferty <rlafferty1@hotmail.com>
2020-03-09 00:22:40 -0400

    Cleaning up duplicate comments

--- src/router/index.ts
+++ src/router/index.ts
@@ -15,10 +15,7 @@ const routes = [
   {
     path: '/contact',
     name: 'Contact',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "contact" */ '@/views/Contact'),
+    component: () => import('@/views/Contact'),
   },
 ];
 

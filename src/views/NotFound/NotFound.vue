f46303cf3e3738045d24a3ff5556c24c3a33e12b
RyanLafferty <rlafferty1@hotmail.com>
2020-03-13 00:21:03 -0400

    Init NotFound page.

---
+++ src/views/NotFound/NotFound.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    TODO
+  </div>
+</template>
+
+<script>
+import NotFoundIcon from '@/assets/svg/404.svg';
+import content from './NotFound.json';
+import styles from './NotFound.module.scss';
+
+export default {
+  data: () => ({
+    content,
+    styles,
+  }),
+  components: {
+    NotFoundIcon,
+  },
+};
+</script>

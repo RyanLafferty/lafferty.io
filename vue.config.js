8c7a07c2e57a62a0dfd012ae46d20a4e07a85e24
RyanLafferty <rlafferty1@hotmail.com>
2020-03-19 19:47:08 -0400

    Updated vue config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/lafferty.io/' : '/',
   chainWebpack: (config) => {
     const svgRule = config.module.rule('svg');
 

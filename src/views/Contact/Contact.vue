c42db9215f0ddd04a3877a663c469bf6b6caf853
RyanLafferty <rlafferty1@hotmail.com>
2020-03-19 23:17:09 -0400

    Updated contact page to use scss modules.

--- src/views/Contact/Contact.vue
+++ src/views/Contact/Contact.vue
@@ -1,56 +1,56 @@
 <template>
-  <div class="container">
-    <iframe :src="content.en.links.resume" class="resume">
+  <div :class="styles.container">
+    <iframe :src="content.en.links.resume" :class="styles.resume">
       Could not load resume
     </iframe>
-    <div class="contactContainer">
-      <div class="titleContainer">
-        <span class="title">
+    <div :class="styles.contactContainer">
+      <div :class="styles.titleContainer">
+        <span :class="styles.title">
           {{content.en.title}}
         </span>
       </div>
-      <div class="linkContainer">
+      <div :class="styles.linkContainer">
         <a
-          class="label"
+          :class="styles.label"
           :href="content.en.contact.email"
           target="_top"
         >
           Email
         </a>
-        <EmailIcon class="icon" alt="Email Icon" />
+        <EmailIcon :class="styles.icon" alt="Email Icon" />
       </div>
-      <div class="linkContainer">
+      <div :class="styles.linkContainer">
         <a
           :href="content.en.links.linkedin"
           target="_blank"
           rel="noopener noreferrer"
-          class="label"
+          :class="styles.label"
         >
           LinkedIn
         </a>
-        <LinkedInIcon class="icon" alt="LinkedIn Icon" />
+        <LinkedInIcon :class="styles.icon" alt="LinkedIn Icon" />
       </div>
-      <div class="linkContainer">
+      <div :class="styles.linkContainer">
         <a
           :href="content.en.links.github"
           target="_blank"
           rel="noopener noreferrer"
-          class="label"
+          :class="styles.label"
         >
           GitHub
         </a>
-        <GitHubIcon class="icon" alt="GitHub Icon" />
+        <GitHubIcon :class="styles.icon" alt="GitHub Icon" />
       </div>
-      <div class="linkContainer">
+      <div :class="styles.linkContainer">
         <a
           :href="content.en.links.npm"
           target="_blank"
           rel="noopener noreferrer"
-          class="label"
+          :class="styles.label"
         >
           npm
         </a>
-        <NpmIcon class="icon" alt="npm Icon" />
+        <NpmIcon :class="styles.icon" alt="npm Icon" />
       </div>
     </div>
   </div>
@@ -62,10 +62,12 @@ import GitHubIcon from '@/assets/svg/github.svg';
 import NpmIcon from '@/assets/svg/npm.svg';
 import LinkedInIcon from '@/assets/svg/linkedin.svg';
 import content from './Contact.json';
+import styles from './Contact.module.scss';
 
 export default {
   data: () => ({
     content,
+    styles,
   }),
   components: {
     EmailIcon,
@@ -75,7 +77,3 @@ export default {
   },
 };
 </script>
-
-<style lang="scss" scoped>
-  @import 'Contact.scss'
-</style>

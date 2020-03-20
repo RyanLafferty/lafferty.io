2c0d4293e63e716d5dfe3d9d9d3c19d5998a1926
RyanLafferty <rlafferty1@hotmail.com>
2020-03-19 22:52:15 -0400

    Added dark mode toggle to site

--- src/components/NavBar/NavBar.vue
+++ src/components/NavBar/NavBar.vue
@@ -1,16 +1,30 @@
 <template>
-  <div class="container">
-    <NavBarOption
-      v-for="option in options"
-      :key="`nav-bar-option-${option.label}`"
-      :link="option.link"
-      :label="option.label"
-    />
+  <div :class="styles.container">
+    <button v-if="darkMode" @click="toggleDarkMode" :class="styles.modeToggle">
+      LIGHT MODE
+    </button>
+    <button v-else @click="toggleDarkMode" :class="styles.modeToggle">
+      DARK MODE
+    </button>
+    <div :class="styles.linkContainer">
+      <NavBarOption
+        v-for="option in options"
+        :key="`nav-bar-option-${option.label}`"
+        :link="option.link"
+        :label="option.label"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 import NavBarOption from '@/components/NavBarOption';
+import {
+  enable as enableDarkMode,
+  disable as disableDarkMode,
+} from 'darkreader';
+import styles from './NavBar.module.scss';
+
 
 export default {
   props: {
@@ -22,12 +36,27 @@ export default {
       }],
     },
   },
+  data: () => ({
+    styles,
+    darkMode: false,
+  }),
+  methods: {
+    toggleDarkMode: function toggleDarkMode() {
+      if (this.darkMode) {
+        disableDarkMode();
+      } else {
+        enableDarkMode({
+          brightness: 100,
+          contrast: 90,
+          sepia: 10,
+        });
+      }
+
+      this.darkMode = !this.darkMode;
+    },
+  },
   components: {
     NavBarOption,
   },
 };
 </script>
-
-<style lang="scss" scoped>
-  @import 'NavBar.scss'
-</style>
